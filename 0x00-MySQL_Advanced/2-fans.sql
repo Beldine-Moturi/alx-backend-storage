2d4c52f6c6a14dac2dbee1eddd6a63708d73e969
Beldine-Moturi <beldinemoturi@gmail.com>
2022-08-31 03:32:20 +0300

    fix errors

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 --  ranks country origins of bands, ordered by the number of (non-unique) fans
-SELECT origin, COUNT(1) AS nb_fans
+SELECT origin, SUM(1) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
