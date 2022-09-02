7af5b2a138b1bf0d2acd6143edf1ec8408fd0482
Beldine-Moturi <beldinemoturi@gmail.com>
2022-09-02 03:17:58 +0300

    fix errors

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,6 +3,11 @@ DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
 UPDATE users
-SET average_score = SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id;
+SET average_score = (
+    SELECT AVG(score)
+    FROM corrections
+    WHERE corrections.user_id = user_id
+    GROUP BY corrections.user_id;
+)
 END$$
 DELIMITER ;
