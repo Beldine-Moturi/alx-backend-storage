85ed142b1dcf063d47b44331c9ccb17324585296
Beldine-Moturi <beldinemoturi@gmail.com>
2022-09-02 03:13:20 +0300

    fix errors

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,27 +2,11 @@
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
--- declare variables
-DECLARE @a INT;
-DECLARE @b FLOAT;
-
---declare and use cursor
-DECLARE my_cursor CURSOR FOR
-    SELECT user_id, AVG(score) AS avg_score
+UPDATE users
+SET average_score = (
+    SELECT AVG(score)
     FROM corrections
-    GROUP BY user_id;
-
-OPEN my_cursor;
-
-FETCH NEXT FROM my_cursor INTO @a, @b;
-
-WHILE @@FETCH_STATUS = 0
-BEGIN
-    UPDATE users
-    SET average_score = @b
-    WHERE id = @a;
-
-    FETCH NEXT FROM my_cursor INTO @a, @b;
-END;
+    WHERE id = user_id
+);
 END$$
 DELIMITER ;
