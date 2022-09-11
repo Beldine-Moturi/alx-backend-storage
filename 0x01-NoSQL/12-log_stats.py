88ace282cc5d102e15f631730975ed98f7da034f
Beldine-Moturi <beldinemoturi@gmail.com>
2022-09-12 02:17:12 +0300

    fix errors

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,18 +3,20 @@
 from pymongo import MongoClient
 
 if __name__ == "__main__":
-    client = MongoClient("mongodb://127.0.0.1:27017")
+    client = MongoClient('mongodb://127.0.0.1:27017')
     logs = client.logs.nginx
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-    print(f"{logs.count_documents({})} logs")
-    print("Methods:")
+    # n = logs.count_documents({})
+    print(f'{logs.count_documents({})} logs')
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print('Methods:')
     for method in methods:
         count = logs.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+        print(f'\tmethod {method}: {count}')
 
-    status = logs.count_documents(
+    status_check = logs.count_documents(
         {"method": "GET", "path": "/status"}
     )
 
-    print(f'{status} status check')
+    print(f'{status_check} status check')
