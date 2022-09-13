183f7999daadbc3c559b8ebd7e352e4215cfe882
Beldine-Moturi <beldinemoturi@gmail.com>
2022-09-13 21:45:06 +0300

    redis basics

---
+++ 0x02-redis_basic/README.md
@@ -0,0 +1,3 @@
+# Redis basics
+Using redis for basic operations
+Using redis as a simple cache
---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Redis basics"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache():
+    """Redis class"""
+
+    def __init__(self) -> None:
+        """Instantiates the class"""
+
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, int, float, bytes]) -> str:
+        """Generates a random key"""
+
+        key = str(uuid.uuid4())
+        self._redis.set(key: data)
+        return key
