27fb47d74fa31742abc78cb5087c43d0d085db80
Beldine-Moturi <beldinemoturi@gmail.com>
2022-09-14 00:40:22 +0300

    fix errors

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -46,6 +46,7 @@ class Cache():
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Stores data in the cache"""
 
